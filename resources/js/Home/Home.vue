646c77767890af3d028aad85964fc8cf406a121c
Vijeshwar Datt <vijeshwar.datt@hosttel.com.au>
2021-04-25 18:20:50 +1200

    Contact form update

--- resources/js/Home/Home.vue
+++ resources/js/Home/Home.vue
@@ -14,10 +14,10 @@
           <h1>About Us</h1>
         </v-card-title>
         <v-card-text>
-          <p class="text-justify">
+          <p>
             GEM (Fiji) Limited, formerly called Graphics Equipments Limited was established in 1972 with a vision of reaching out to customers and providing business solutions. We are a unique family business providing business solutions for organizations to be more efficient, improve performance and provide better results. We work with organizations in every industry in the public and private sectors and of every size from start-ups to multinationals.
           </p>
-          <p class="text-justify">
+          <p>
             GEM IT SOLUTIONS is the ONLY PLATINUM PARTNER of DELL in Fiji with Authorized Service Centre here to cater to all warranty and service needs, making sure our clients get the best after-sales services.
           </p>
         </v-card-text>
@@ -29,7 +29,7 @@
           <v-row align="center">
             <v-col cols="12" md="6" class="text-md-end text-center">
               <h1 class="py-4">GEM Office Products</h1>
-              <p class="text-justify">GEM Office Products is a customer focused business solutions company with the prime objective of providing each of our customers with an unparalleled level of service, coupled with the most competitive prices in the industry. In order to differentiate ourselves from our competitors, we have taken positive steps to ensure that all our customers are treated with equal care, from large corporate accounts through to small office / home office customers.</p>
+              <p>GEM Office Products is a customer focused business solutions company with the prime objective of providing each of our customers with an unparalleled level of service, coupled with the most competitive prices in the industry. In order to differentiate ourselves from our competitors, we have taken positive steps to ensure that all our customers are treated with equal care, from large corporate accounts through to small office / home office customers.</p>
             </v-col>
             <v-col cols="12" md="6" v-if="$vuetify.breakpoint.mdAndUp">
               <v-img :src="require('../../assets/images/home-op.svg')" contain></v-img>
@@ -39,12 +39,12 @@
             </v-col>
             <v-col cols="12" md="6" class="text-md-start text-center">
               <h1 class="py-4">GEM IT Solutions</h1>
-              <p class="text-justify">GEM IT Solutions is the IT arm of GEM (Fiji) Limited. GEM IT Solutions primary objective is to help our customers achieve their business goals with the application of pertinent technology solutions. Our business model combines industry leading technology accreditations, diligent account managers, supported by a large team of experienced technical consultants and engineers. Our success has come from providing our clients with unrivalled technical expertise along with extremely high service levels.</p>
-              <p class="text-justify">GEM IT Technical Engineers have a wealth of experience in solution design and deployment, and have the confidence in technology required to deliver integrated solutions that meet your business demands. With our comprehensive portfolio of leading technology partners, our in-house Dell-certified expert team of technical resource are here to meet your day-to-day business challenges.</p>
+              <p>GEM IT Solutions is the IT arm of GEM (Fiji) Limited. GEM IT Solutions primary objective is to help our customers achieve their business goals with the application of pertinent technology solutions. Our business model combines industry leading technology accreditations, diligent account managers, supported by a large team of experienced technical consultants and engineers. Our success has come from providing our clients with unrivalled technical expertise along with extremely high service levels.</p>
+              <p>GEM IT Technical Engineers have a wealth of experience in solution design and deployment, and have the confidence in technology required to deliver integrated solutions that meet your business demands. With our comprehensive portfolio of leading technology partners, our in-house Dell-certified expert team of technical resource are here to meet your day-to-day business challenges.</p>
             </v-col>
             <v-col cols="12" md="6" class="text-md-end text-center">
               <h1 class="py-4">GEM Visual Concepts</h1>
-              <p class="text-justify">GEM Visual Concepts understand that the success of a company depends in part to creating visibility in the market place. We are a fully-fledged design, marketing and print production setup. Our products and services include design concepts, wide format printing, promotional displays, sign boards, desktop publishing, promotional products and I.D. card printing and related accessories. Our team strives to develop creative concepts that accurately capture our clients’ communication and marketing requirements. We value simplicity and emphasize on friendly and personal service. Let us enhance your business with outstanding visual and communication concepts.</p>
+              <p>GEM Visual Concepts understand that the success of a company depends in part to creating visibility in the market place. We are a fully-fledged design, marketing and print production setup. Our products and services include design concepts, wide format printing, promotional displays, sign boards, desktop publishing, promotional products and I.D. card printing and related accessories. Our team strives to develop creative concepts that accurately capture our clients’ communication and marketing requirements. We value simplicity and emphasize on friendly and personal service. Let us enhance your business with outstanding visual and communication concepts.</p>
             </v-col>
             <v-col cols="12" md="6" v-if="$vuetify.breakpoint.mdAndUp">
               <v-img :src="require('../../assets/images/home-vc.svg')" contain></v-img>
